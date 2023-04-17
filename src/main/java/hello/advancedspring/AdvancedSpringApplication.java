249536b6c8f28ddccfcfc5b631db8f49ca415d34
player-geun <player_geun@naver.com>
2023-04-18 08:08:43 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/hello/advancedspring/AdvancedSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.advancedspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedSpringApplication.class, args);
+    }
+
+}
