da088babdc3adce9778a657287ab1684895604c5
player-geun <player_geun@naver.com>
2023-04-19 14:10:46 +0900

    feat : 간단한 주문 기능

---
+++ src/main/java/hello/advancedspring/v0/OrderRepositoryV0.java
@@ -0,0 +1,25 @@
+package hello.advancedspring.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        //저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);    // 주문 저장하는데 1초가 걸린다고 가정
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
