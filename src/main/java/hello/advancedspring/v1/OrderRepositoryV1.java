1cc7ecfb0915d8f7802d44e28e86a85a32695b7f
player-geun <player_geun@naver.com>
2023-04-19 14:11:44 +0900

    feat : 로그 추적기를 적용한 v1 버전의 주문 기능

---
+++ src/main/java/hello/advancedspring/v1/OrderRepositoryV1.java
@@ -0,0 +1,39 @@
+package hello.advancedspring.v1;
+
+import hello.advancedspring.trace.TraceStatus;
+import hello.advancedspring.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+
+            //저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);    // 주문 저장하는데 1초가 걸린다고 가정
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/hello/advancedspring/v1/OrderServiceV1.java
@@ -0,0 +1,26 @@
+package hello.advancedspring.v1;
+
+import hello.advancedspring.trace.TraceStatus;
+import hello.advancedspring.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
