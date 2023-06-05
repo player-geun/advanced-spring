f645a52dbaf5df129b8496c70c331243155c4e18
player-geun <player_geun@naver.com>
2023-06-05 15:48:12 +0900

    feat : 실제 주문 로직에 추상 템플릿이 적용된 LocalTrace 구현

---
+++ src/main/java/hello/advancedspring/v4/OrderRepositoryV4.java
@@ -0,0 +1,37 @@
+package hello.advancedspring.v4;
+
+import hello.advancedspring.trace.TraceStatus;
+import hello.advancedspring.trace.logtrace.LogTrace;
+import hello.advancedspring.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+//저장 로직
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/hello/advancedspring/v4/OrderServiceV4.java
@@ -0,0 +1,26 @@
+package hello.advancedspring.v4;
+
+import hello.advancedspring.trace.TraceStatus;
+import hello.advancedspring.trace.logtrace.LogTrace;
+import hello.advancedspring.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderService.orderItem()");
+    }
+}
